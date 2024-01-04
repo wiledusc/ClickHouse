f6ca2d52206613176048abb500ac0f68a7ae86bf
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-01-04 00:05:57 +0000

    Refreshable materialized views improvements

--- src/Interpreters/InterpreterSystemQuery.cpp
+++ src/Interpreters/InterpreterSystemQuery.cpp
@@ -645,6 +645,9 @@ BlockIO InterpreterSystemQuery::execute()
         case Type::REFRESH_VIEW:
             getRefreshTask()->run();
             break;
+        case Type::WAIT_VIEW:
+            getRefreshTask()->wait();
+            break;
         case Type::CANCEL_VIEW:
             getRefreshTask()->cancel();
             break;
@@ -1336,6 +1339,7 @@ AccessRightsElements InterpreterSystemQuery::getRequiredAccessForDDLOnCluster() 
             break;
         }
         case Type::REFRESH_VIEW:
+        case Type::WAIT_VIEW:
         case Type::START_VIEW:
         case Type::START_VIEWS:
         case Type::STOP_VIEW:
